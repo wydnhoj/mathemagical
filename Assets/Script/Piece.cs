5951c62581e99e257cfe103add017944074591f6
John Ross Ferrancullo <ferrancullojohnross04@gmail.com>
2023-11-28 17:23:46 +0800

    Add Valid Moves function

--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isWhite;
+    public bool isKing;
+    public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2){
+        //if you are moving on top of another piece
+        if(board[x2,y2] != null){
+            return false;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int deltaMove = Mathf.Abs(x1-x2);
+        int deltaMoveY = y1-y2;
+
+        if(isWhite || isKing){
+            if(deltaMove == 1){
+                if(deltaMoveY == 1){
+                    return true;
+                }
+            }
+            else if(deltaMove == 2){
+                if(deltaMoveY == 2){
+                    Piece p = board[(x1+x2)/2, (y1+y2)/2];
+                    if(p != null && p.isWhite != isWhite){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        if(!isWhite || isKing){
+            if(deltaMove == 1){
+                if(deltaMoveY == -1){
+                    return true;
+                }
+            }
+            else if(deltaMove == 2){
+                if(deltaMoveY == -2){
+                    Piece p = board[(x1+x2)/2, (y1+y2)/2];
+                    if(p != null && p.isWhite != isWhite){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
