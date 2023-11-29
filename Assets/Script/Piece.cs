7f1da2352f48e14537324c5094dffbb91ac0bfd5
John Ross Ferrancullo <ferrancullojohnross04@gmail.com>
2023-11-29 11:15:32 +0800

    King now can MOVE and EAT BACKWARDS

--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -1,11 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
     public bool isWhite;
     public bool isKing;
+    
+    public bool isForceToMove(Piece[,] board, int x, int y){        //FIX THE JUMPING BACKWARDS OF THE KING (UPDATE ALREADY FIXED!!)
+        if(isWhite || isKing){
+            //top left
+            if(x>=2 && y<=5){
+                Piece p = board[x-1,y+1];
+                //if opponent piece is present
+                if(p!=null && p.isWhite!=isWhite){
+                    //check if it can eat it and land on it
+                    if(board[x-2,y+2]==null){
+                        return true;
+                    }
+                }
+            }
+
+            //top right
+            if(x<=5 && y<=5){
+                Piece p = board[x+1,y+1];
+                //if opponent piece is present
+                if(p!=null && p.isWhite!=isWhite){
+                    //check if it can eat it and land on it
+                    if(board[x+2,y+2]==null){
+                        return true;
+                    }
+                }
+            }
+            //top left king
+            if(isKing){
+                if(x>=2 && y<=5){
+                    Piece p = board[x-1,y+1];
+                    if(p!=null && p.isWhite!=isWhite){
+                        if(board[x-2,y-2]==null){
+                            return true;
+                        }
+                    }
+                }
+                //top right king
+                if(x>=2 && y<=5){
+                    Piece p = board[x+2,y-2];
+                    if(p!=null && p.isWhite!=isWhite){
+                        if(board[x+2,y-2]==null){
+                            return true;
+                        }
+                    }
+                }
+            }
+
+        }
+        if(!isWhite || isKing){
+            //bottom left
+            if(x>=2 && y>=2){
+                Piece p = board[x-1,y-1];
+                //if opponent piece is present
+                if(p!=null && p.isWhite!=isWhite){
+                    //check if it can eat it and land on it
+                    if(board[x-2,y-2]==null){
+                        return true;
+                    }
+                }
+            }
+
+            //bottom right
+            if(x<=5 && y>=2){
+                Piece p = board[x+1,y-1];
+                //if opponent piece is present
+                if(p!=null && p.isWhite!=isWhite){
+                    //check if it can eat it and land on it
+                    if(board[x+2,y-2]==null){
+                        return true;
+                    }
+                }
+            }
+            //bottom left king
+            if(isKing){
+                if(x>=2 && y>=2){
+                    Piece p = board[x-1,y-1];
+                    if(p!=null && p.isWhite!=isWhite){
+                        if(board[x-2,y+2]==null){
+                            return true;
+                        }
+                    }
+                }
+                //bottom right king
+                if(x<=5 && y>=2){
+                    Piece p = board[x+1,y-1];
+                    if(p!=null && p.isWhite!=isWhite){
+                        if(board[x+2,y+2]==null){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    //make look for the possible move on the pieces and if it isKing it can move backwards
     public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2){
         //if you are moving on top of another piece
         if(board[x2,y2] != null){
@@ -15,7 +113,7 @@ public class Piece : MonoBehaviour
         int deltaMove = Mathf.Abs(x1-x2);
         int deltaMoveY = y2-y1;
 
-        if(isWhite || isKing){
+        if(isWhite){
             if(deltaMove == 1){
                 if(deltaMoveY == 1){
                     return true;
@@ -29,9 +127,24 @@ public class Piece : MonoBehaviour
                     }
                 }
             }
+            if(isKing){
+                if(deltaMove == 1){
+                    if(deltaMoveY == -1){
+                        return true;
+                    }
+                }
+                else if(deltaMove == 2){
+                    if(deltaMoveY == -2){
+                        Piece p = board[(x1+x2)/2, (y1+y2)/2];
+                        if(p != null && p.isWhite != isWhite){
+                            return true;
+                        }
+                    }
+                }
+            }
         }
 
-        if(!isWhite || isKing){
+        if(!isWhite){
             if(deltaMove == 1){
                 if(deltaMoveY == -1){
                     return true;
@@ -42,6 +155,21 @@ public class Piece : MonoBehaviour
                     Piece p = board[(x1+x2)/2, (y1+y2)/2];
                     if(p != null && p.isWhite != isWhite){
                         return true;
+                    }
+                }
+            }
+            if(isKing){
+                if(deltaMove == 1){
+                    if(deltaMoveY == 1){
+                        return true;
+                    }
+                }
+                else if(deltaMove == 2){
+                    if(deltaMoveY == 2){
+                        Piece p = board[(x1+x2)/2, (y1+y2)/2];
+                        if(p!= null && p.isWhite != isWhite){
+                            return true;
+                        }
                     }
                 }
             }
